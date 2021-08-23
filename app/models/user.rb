d4f4bb570f4bfc8d4204ed1453cf64d488831a6e
Stuart Coombe <s_coombe89@hotmail.co.uk>
2021-08-23 16:29:23 +0100

    killer model / user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :spookings, dependent: :destroy
+  has_many :killers, dependent: :destroy
+  has_many :killers, through: :spookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
