4975f2b7b696fe76aa8e83c8265c60c5acd59539
jonathan bell <jonnybell123@hotmail.com>
2021-08-25 10:52:15 +0100

    changed killer prices and added image relative paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 nemesis = Killer.create!(
   [ user_id: 1,
     name: 'The Tyrant',
